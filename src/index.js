32be8f4bb762b580264a8c5606bac5ef834dd1f0
Seniorgrok <68947554+Seniorgrok@users.noreply.github.com>
2021-03-16 02:51:32 +0300

    fix: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (!n) {
+        return null;
+    }
+    let nstring = n.toString();
+    if (n < 0) {
+        nstring = nstring.substring(1);
+    }
+    return nstring.split("").reverse().join("");
 }
